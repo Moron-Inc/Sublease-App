4963c53449f694297c36e43f3aee9816964fc706
arjunpatel01 <55220892+arjunpatel01@users.noreply.github.com>
2021-04-04 21:01:00 -0500

    Fixed background image

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,35 @@
 import React from 'react';
-import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import { Form, FormControl } from 'react-bootstrap';
 import styled from 'styled-components';
-import picture from './images/MarioKartBanner.jpg'
 
 const Wrapper = styled.div`
   margin-top: 1em;
-  margin-left: 6em;
-  margin-right: 6em;
+  width: 100%;
+  height: 100vh;
 `;
 
-const Styles = styled.div`
-  .navbar { background-color: white; }
-  a, .navbar-nav, .navbar-light .nav-link {
-    color: black;
-    &:hover { color: blue; }
-  }
-  .navbar-brand {
-    font-size: 1.4em;
-    color: black;
-    &:hover { color: blue; }
-  }
-  .form-center {
-    position: absolute !important;
-    left: 25%;
-    right: 25%;
-  }
-`;
+// const Styles = styled.div`
+//   .navbar { background-color: white; }
+//   a, .navbar-nav, .navbar-light .nav-link {
+//     color: black;
+//     &:hover { color: blue; }
+//   }
+//   .navbar-brand {
+//     font-size: 1.4em;
+//     color: black;
+//     &:hover { color: blue; }
+//   }
+//   .form-center {
+//     position: absolute !important;
+//     left: 25%;
+//     right: 25%;
+//   }
+// `;
 
 
 export const Home = () => (
   <Wrapper>
-    <h2>Home</h2>
-    <div className="container">
+    <div className="custom-container">
       <Form className="form-center">
         <span><FormControl type="text" placeholder="Search" className="" /></span>
       </Form>
